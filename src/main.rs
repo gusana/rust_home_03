ed22a304ddc8c35d15d3102ce22e9c7c4f009e75
gusana <gus.anyia@gmail.com>
2023-12-28 15:24:42 +0100

    fixes baes on comments

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,28 @@
-use modules_and_tests::default_mod::{
-                                        default_signed_counter,
-                                        default_unsigned_counter,
-                                        next_signed,
-                                        next_unsigned,
-                                        prev_signed
-                                    };
-
-use modules_and_tests::scalar_mod::{
-                                        default_pair,
-                                        pair_vector_sum,
-                                        pair_scalar_sum
-
+mod counters;
+mod pair;
+mod vector;
+use counters::counters::{
+    default_signed_counter, default_unsigned_counter, next_signed, next_unsigned, prev_signed,
 };
 
-use modules_and_tests::vector_mod::{    
-                                        default_vec3,
-                                        vec3_vector_sum,
-                                        vec3_scalar_sum
-};
+use pair::pair::{default_pair, pair_scalar_sum, pair_vector_sum};
+
+use vector::vector::{default_vec3, vec3_scalar_sum, vec3_vector_sum};
 
 fn main() {
-assert_eq!(0_usize, default_unsigned_counter());
-assert_eq!(0_isize, default_signed_counter());
-assert_eq!(2, next_signed(1));
-assert_eq!(2, next_unsigned(1));
-assert_eq!(2, prev_signed(3));
+    assert_eq!(0_usize, default_unsigned_counter());
+    assert_eq!(0_isize, default_signed_counter());
+    assert_eq!(2, next_signed(1));
+    assert_eq!(2, next_unsigned(1));
+    assert_eq!(2, prev_signed(3));
 
-let def_vec: Vec<i32> = vec![0, 0, 0];
-assert_eq!(def_vec, default_vec3());
-assert_eq!([2,2,3], vec3_vector_sum([1, 1, 2], [1, 1, 1]));
-assert_eq!(8, vec3_scalar_sum([0, 2, 1], [1, 1, 3]));
+    let def_vec: Vec<i32> = vec![0, 0, 0];
+    assert_eq!(def_vec, default_vec3());
+    assert_eq!([2, 2, 3], vec3_vector_sum([1, 1, 2], [1, 1, 1]));
+    assert_eq!(8, vec3_scalar_sum([0, 2, 1], [1, 1, 3]));
 
-let def_pair = (0, 0);
-assert_eq!(def_pair, default_pair());
-assert_eq!((2, 2), pair_vector_sum((1, 1), (1, 1)));
-assert_eq!(4, pair_scalar_sum((1, 1), (1, 1)))
-
-    
+    let def_pair = (0, 0);
+    assert_eq!(def_pair, default_pair());
+    assert_eq!((2, 2), pair_vector_sum((1, 1), (1, 1)));
+    assert_eq!(4, pair_scalar_sum((1, 1), (1, 1)))
 }
---
+++ src/vector.rs
@@ -0,0 +1,45 @@
+pub mod vector {
+    pub const VEC3_LEN: usize = 3;
+    pub type Vec3 = [i32; VEC3_LEN];
+
+    pub fn default_vec3() -> Vec3 {
+        [0; 3]
+    }
+
+    pub fn vec3_vector_sum(a: Vec3, b: Vec3) -> Vec3 {
+        let mut c = default_vec3();
+        for i in 0..3 {
+            c[i] = a[i] + b[i];
+        }
+        c
+    }
+
+    pub fn vec3_scalar_sum(a: Vec3, b: Vec3) -> i32 {
+        let mut c = 0;
+        for i in 0..VEC3_LEN {
+            c += a[i] + b[i];
+        }
+        c
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn check_default_vector() {
+        use vector::default_vec3;
+        let def_vec: Vec<i32> = vec![0, 0, 0];
+        assert_eq!(def_vec, default_vec3());
+    }
+    #[test]
+    fn check_vector_sum() {
+        use vector::vec3_vector_sum;
+        assert_eq!([2, 2, 3], vec3_vector_sum([1, 1, 2], [1, 1, 1]));
+    }
+    #[test]
+    fn check_scalar_sum() {
+        use vector::vec3_scalar_sum;
+        assert_eq!(8, vec3_scalar_sum([0, 2, 1], [1, 1, 3]));
+    }
+}
